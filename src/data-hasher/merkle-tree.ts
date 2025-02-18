20912ac33d8c357561b98f749cc8a4c471dfa597
Sujeet Silwal <silwalsujeet149@gmail.com>
2025-02-18 15:06:52 +0545

    Fix issue with incorrect duplication of node

--- src/data-hasher/merkle-tree.ts
+++ src/data-hasher/merkle-tree.ts
@@ -12,36 +12,35 @@ export class MerkleTree {
   private readonly leaves: Array<LeafHash>;
   private readonly hashFn: HashFn;
 
-  constructor(leaves: Array<LeafHash>, sortLeaves: boolean, hashFn?: HashFn) {
+  constructor(leaves: Array<LeafHash>, hashFn?: HashFn) {
     this.leaves = leaves;
     this.hashFn = hashFn ? hashFn : hashMerge;
 
     // if the no of leaves is odd, we duplicate the last leaves to make it even
     if (this.leaves.length & 1)
       this.leaves.push(this.leaves[this.leaves.length - 1]);
-    if (sortLeaves) this.leaves.sort();
   }
 
   calculateRoot(): LeafHash {
     if (this.leaves.length == 0) throw new Error("Leaf count is zero");
     if (this.leaves.length === 1) return this.leaves[0];
 
-    const hashes = [...this.leaves];
-    let hashCount = hashes.length;
+    let inputs = [...this.leaves];
 
-    while (hashCount > 1) {
-      if (hashes.length & 1) {
-        hashes.push(hashes[hashes.length - 1]);
-        hashCount += 1;
+    while (inputs.length > 1) {
+      if (inputs.length & 1) {
+        // Duplicate the last element
+        inputs.push(inputs[inputs.length - 1]);
       }
 
-      for (let leaf = 0; leaf < hashCount; leaf += 2) {
-        const left = hashes[leaf];
-        const right = hashes[leaf + 1];
-        hashes[leaf / 2] = this.hashFn(left, right);
+      let outputs = [];
+      for (let leaf = 0; leaf < inputs.length; leaf += 2) {
+        const left = inputs[leaf];
+        const right = inputs[leaf + 1];
+        outputs.push(this.hashFn(left, right));
       }
-      hashCount /= 2;
+      inputs = outputs;
     }
-    return hashes[0];
+    return inputs[0];
   }
 }
